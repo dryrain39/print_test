a817ae3fd6d6feb4339e1164859a91035746229a
Geonu Ahn <akwoo1@naver.com>
2019-08-13 15:05:38 +0900

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,6 @@
+@echo off
+echo --------------------------------------------------------
+echo Auto print script. 작업이 끝나면 창이 자동으로 닫힙니다.
+echo --------------------------------------------------------
+wget --output-document=C:\print_test\cat.jpg "https://cataas.com/cat" 
+rundll32 C:\WINDOWS\system32\shimgvw.dll,ImageView_PrintTo "C:\print_test\cat.jpg" "your_printer_name"
